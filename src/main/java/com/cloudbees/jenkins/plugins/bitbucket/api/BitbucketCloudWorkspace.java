9161d5730d79fcd2a88bd36572c01c2984f4e3f9
Oliver Lockwood <oliver.lockwood@cantab.net>
2021-08-26 11:38:16 +0100

    Fix checkstyle complaint

--- src/main/java/com/cloudbees/jenkins/plugins/bitbucket/api/BitbucketCloudWorkspace.java
+++ src/main/java/com/cloudbees/jenkins/plugins/bitbucket/api/BitbucketCloudWorkspace.java
@@ -2,7 +2,6 @@ package com.cloudbees.jenkins.plugins.bitbucket.api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
 import java.util.List;
 import java.util.Map;
 
